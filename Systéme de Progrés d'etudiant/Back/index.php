7f5ea086b891400179538b1ba2cc86b54c12f27e
Nour <khediri.noureslem@esprit.tn>
2024-11-10 22:50:47 +0100

    Add some Major Updates about the website view
    ka prtie front we khw

--- Systéme de Progrés d'etudiant/Back/index.php
+++ Systéme de Progrés d'etudiant/Back/index.php
@@ -4,12 +4,42 @@
     <meta charset="UTF-8">
     <link rel="icon" type="image/png" href="../Ressources/logo.png">
     <link rel="stylesheet" href="style.css">
-    <script src="sys.js"></script>
+    <script src="sys.js" defer></script> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EduLivre</title>
-</title>
 </head>
 <body>
-   
+    <span class="bckg"></span>
+    <header>
+        <h1>Dashboard</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="javascript:void(0);" data-title="Projects">Progrés</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0);" data-title="Team">Courses</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="title">
+            <h2>Progrés</h2>
+            <a href="javascript:void(0);">Hello Nour!</a>
+        </div>
+
+        <section class="larg">
+            <div>
+                <h3>Koussay Ghanmi <span class="entypo-down-open"></span></h3>
+                <p></p>
+            </div>
+            <div>
+                <h3>Rayen Mezni <span class="entypo-down-open"></span></h3>
+                <p>Lom</p>
+            </div>
+
+        </section>
+    </main>
 </body>
 </html>
