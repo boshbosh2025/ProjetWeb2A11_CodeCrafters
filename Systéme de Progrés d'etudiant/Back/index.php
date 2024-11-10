d44cab5b27f64c016e12eaba345402aff93badd5
Nour <khediri.noureslem@esprit.tn>
2024-11-10 18:35:05 +0100

    add the needed files to create website
    enter code if u want to see what is new
    for colleges only and instructors
    :anger:

---
+++ Systéme de Progrés d'etudiant/Back/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="../Ressources/logo.png">
+    <link rel="stylesheet" href="style.css">
+    <script src="sys.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Systéme de Progrés</title>
+</head>
+<body>
+   
+</body>
+</html>
