c864819571b2700876781336f0a3339c9c27f4fa
Nour <khediri.noureslem@esprit.tn>
2024-11-11 21:54:35 +0100

    added stuff

---
+++ Forum/View/Chatroom.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EduLivre</title>
+    <link rel="shortcut icon" href="../View/Resource/logo.png" type="image/x-icon">
+</head>
+<body>
+        <?php
+        include_once 'login.php';
+        include_once 'sign.php';
+
+
+        
+    ?>
+</body>
+</html>
