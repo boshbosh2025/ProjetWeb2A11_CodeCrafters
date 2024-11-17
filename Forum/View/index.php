efbc53a014d0279024a95e66d9c2f9c512644121
Nour <khediri.noureslem@esprit.tn>
2024-11-17 18:42:04 +0100

    updatye

--- Forum/View/index.php
+++ Forum/View/index.php
@@ -1,9 +1,11 @@
 <?php
 session_start();
 include_once "bd.php";
+include_once "addSujet_class.php" ;
+include_once "connexion_class.php";
+
 
 if (!isset($_SESSION['id'])) {
-    // Redirect to signup.php if the user is not logged in
     header("Location: signup.php");
     exit();
 }
@@ -24,41 +26,43 @@ $bd = bdd();
     <div id="Cforum">
         <p>
             <?php
-            if (isset($_GET['categorie'])) { // If category is set
-                echo "Bienvenue, " . htmlspecialchars($_SESSION['username']);
+            echo "Bienvenue, " . htmlspecialchars($_SESSION['username']) . "!";
 
+            if (isset($_GET['categorie'])) {
                 $_GET['categorie'] = htmlspecialchars($_GET['categorie']);
                 ?>
                 <a href="addSujet.php">Ajouter un sujet</a>
                 <div class="categories">
-                    <h1><?php echo $_GET['sujet']; ?></h1>
+                    <h1><?php echo htmlspecialchars($_GET['categorie']); ?></h1>
                 </div>
                 <?php
-            } else if (isset($_GET['sujet'])) { // If subject is set
+            } elseif (isset($_GET['sujet'])) {
                 $_GET['sujet'] = htmlspecialchars($_GET['sujet']);
                 ?>
                 <div class="categories">
-                    <h1><?php echo $_GET['sujet']; ?></h1>
+                    <h1><?php echo htmlspecialchars($_GET['sujet']); ?></h1>
                 </div>
                 <a href="addSujet.php">Ajouter un sujet</a>
                 <?php
-                $req = $bd->query("SELECT * FROM postsujet WHERE sujet = :sujet");
-                $req->execute(array('sujet' => $_GET['sujet']));
+                $req = $bd->prepare("SELECT * FROM postsujet WHERE sujet = :sujet");
+                $req->execute(['sujet' => $_GET['sujet']]);
+
                 while ($rep = $req->fetch()) {
                     ?>
                     <div class="post">
-                        <?php
-                        $req3 = $bd->prepare("SELECT * FROM compte WHERE id = :id");
-                        $req3 = $bd->execute(array('id'=>$rep['propri']));
-                        $compte = $req3->fetch();
-                        echo $compte['username'];
-                        echo ': <br>';
-                        echo htmlspecialchars($rep['contenu']); ?>
+                        <?php                                           //erorr naffiche pas le user 
+                        $req3 = $bd->prepare("SELECT * FROM user WHERE idPrimaire = :idPrimaire");
+                        $req3->execute(['idPrimaire' => $rep['propri']]);
+                        $user = $req3->fetch();
+                            echo htmlspecialchars($user['username']) . ': <br>'; 
+                            echo htmlspecialchars($rep['contenu']);
+                    
+                        ?>
                     </div>
                     <?php
                 }
-            } else { // Default case if neither category nor subject is set
-                echo "Bienvenue, " . htmlspecialchars($_SESSION['username']) . ", sélectionnez le group chat : ";
+            } else {
+                echo "Sélectionnez le group chat : ";
                 $req = $bd->query('SELECT * FROM categorie');
                 while ($reponse = $req->fetch()) {
                     ?>
